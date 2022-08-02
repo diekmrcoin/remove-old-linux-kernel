cf7a4ebd319c0cb338b7c4dcd43e325a761773ec
Diego Maroto <diego.diemg@gmail.com>
2022-08-02 17:36:59 +0200

    Added bash code and removed python gitignore

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # remove-old-linux-kernel
-Bash and python script that removes old linux kernels
+Bash script that removes old linux kernels
+
+- `sudo cp src/main.sh /usr/bin/remove-old-kernels`
+- `sudo chmod +x /usr/bin/remove-old-kernels`
+- `sudo remove-old-kernels`
+  - See the `/boot` usage percentage
+  - Enter the number of kernels you want to leave installed
+  - Decide to proceed or not with the `apt purge` of the output displayed
+  - See the new `/boot` usage percentage
+  - Decide to proceed or not with the `apt autoremove` to clean the kernel headers left installed
+  - Done
---
+++ src/main.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+BOOTUSAGE=$(df -h | grep -E /boot$ | awk '{print $5}')
+echo -e "Usage of /boot: $BOOTUSAGE\n"
+
+read -p "Enter the number of kernels you want to leave installed (default:3) " AMOUNT
+
+if [ -z "$AMOUNT" ]; then
+    AMOUNT=3
+fi
+
+is_int () { test "$@" -eq "$@" 2> /dev/null; }
+if ! is_int $AMOUNT; then
+    echo "Please enter a number"
+    exit 1
+fi
+
+KERNELLIST=$(dpkg -l linux-image-\* | grep -E '^ii\s+linux-image-[1-9]+' | awk '{print $2}' | sort -Vr)
+echo -e "Kernels available:\n$KERNELLIST\n"
+KERNELSTOUNINSTALL=$(echo -e "$KERNELLIST" | tail -n $AMOUNT)
+echo -e "Kernels to uninstall:\n$KERNELSTOUNINSTALL\n"
+
+read -p "Proceed? (y/n)" PROCEED
+if [ "$PROCEED" != "y" ]; then
+    echo "Aborting"
+    exit 1
+fi
+
+apt-get purge $KERNELSTOUNINSTALL
+
+BOOTUSAGE=$(df -h | grep -E /boot$ | awk '{print $5}')
+echo -e "Usage of /boot after the uninstall: $BOOTUSAGE\n"
+
+read -p "Do you want to execute auto-remove to clean the headers? (y/n)" PROCEED
+if [ "$PROCEED" == "y" ]; then
+    apt-get autoremove
+fi
+
+apt autoremove
+
+echo -e "\nDone\n"
